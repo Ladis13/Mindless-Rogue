a0edca1d9e44ef85ab3f7919d18e235b28b78e4b
Ladis13 <ladis13.bodele@gmail.com>
2021-06-15 17:15:39 +0200

    trying out rendering and textures

--- src/maintest.c
+++ src/maintest.c
@@ -7,9 +7,30 @@ int main(int argc, char* argv[]) {
         printf("error initializing SDL: %s\n", SDL_GetError());
     }
     SDL_Window* win = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, 0);
+    SDL_Surface* surface = IMG_Load("src/1.jpg");
+    if(surface == NULL)
+        printf("BANANE\n");
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_RenderClear(renderer);
+
+    SDL_Rect rect;
+    SDL_QueryTexture(tex, NULL, NULL, &rect.w, &rect.h);
+
+    rect.x = (1000 - rect.w) / 2;
+    rect.y = (1000 - rect.h) / 2;
+
+    SDL_RenderCopy(renderer, tex, NULL, &rect);
+
+    // SDL_SetRenderDrawColor(renderer, 255,0,0, SDL_ALPHA_OPAQUE);
+    // SDL_RenderDrawLine(renderer, 0,500, 1000,500);
+    // SDL_RenderDrawLine(renderer, 500,0, 500,1000);
+    SDL_RenderPresent(renderer);
 
     int close = 0;
-    int y;
+    int x, y;
     while (!close) {
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
@@ -17,16 +38,34 @@ int main(int argc, char* argv[]) {
                 case SDL_QUIT:
                     close = 1;
                     break;
+                case SDL_KEYDOWN:
+                    switch (e.key.keysym.scancode) {
+                        case SDL_SCANCODE_ESCAPE:
+                            close = 1;
+                            break;
+                        default :
+                            break;
+                    }
                 case SDL_MOUSEBUTTONDOWN:
-                    SDL_GetMouseState(NULL, &y);
+                    SDL_GetMouseState(&x, &y);
                     if (y >500)
-                        printf("click bottom\n");
+                        if (x > 500)
+                            printf("click bottom right\n");
+                        else
+                            printf("click bottom left\n");
                     else
-                        printf("click top\n");
+                        if (x > 500)
+                            printf("click top right\n");
+                        else
+                            printf("click top left\n");
+                default:
+                    break;
             }
         }
     }
 
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_Quit();
     return 0;
